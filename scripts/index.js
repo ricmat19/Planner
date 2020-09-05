300c4469f06a1174ac23d0d7f7e681cc4c7c9747
Ricky Del Cueto <ric19arthouse@gmail.com>
2020-09-05 17:25:01 -0400

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+let inputText = document.querySelector(".to-do-input");
+let toDoList = document.querySelector(".to-do-list");
+let addButton = document.querySelector(".to-do-button");
+
+addButton.addEventListener("click", addToDo);
+
+inputText.addEventListener("keyup", function enterToDo(){
+    if(event.keyCode === 13){
+        addToDo();
+    }
+});
+
+function addToDo(){
+    toDoItem = document.querySelector(".to-do-item");
+
+    if (inputText.value !== ""){
+        toDoList.innerHTML += '<div class="grid to-do-item"><div class="to-do-item-name">' + inputText.value + '</div><div class="to-do-delete">X</div></div>'
+    }
+
+    inputText.value = "";
+
+};
+
+toDoList.addEventListener("click", function deleteToDo(e){
+    
+    let elementClicked = e.target;
+
+    //
+    if(elementClicked.classList[0] === "to-do-delete"){
+
+        let removedToDo = elementClicked.parentElement;
+
+        removedToDo.remove();
+    }
+        
+});
+
+
+
+
