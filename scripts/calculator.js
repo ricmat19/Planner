fb92cb98c6635b294672d8d88d3036d756675081
Ricky Del Cueto <ric19arthouse@gmail.com>
2020-09-08 23:39:01 -0400

    Calculator JS Setup

---
+++ scripts/calculator.js
@@ -0,0 +1,18 @@
+let zero = document.querySelector(".zero");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".minus");
+let multiply = document.querySelector(".multiply");
+let divide = document.querySelector(".divide");
+let equal = document.querySelector(".equal");
+let clear = document.querySelector(".clear");
+
+
