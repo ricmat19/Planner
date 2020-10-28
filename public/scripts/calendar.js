37d387e31e6a64ed86ec1009a4b6d2e6bdc73a8c
Ricky Del Cueto <ric19arthouse@gmail.com>
2020-10-27 21:21:13 -0400

    Added Navbar

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -354,3 +354,21 @@ function displayMonth(selectedMonth, change){
             break;
     }
 }
+
+// let button = document.querySelector(".modal-button");
+// let modal = document.querySelector(".modal");
+// let modalClose = document.querySelector(".modal-close");
+
+// button.addEventListener("click", function(){
+//     modal.style.display = "grid";
+// })
+
+// modalClose.addEventListener("click", function(){
+//     modal.style.display = "none";
+// })
+
+// window.addEventListener("click", function(e){
+//     if(e.target === modal){
+//         modal.style.display = "none";
+//     }
+// })
