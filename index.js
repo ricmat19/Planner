aac74d9fc9a5760b2e0605f867b9aebb0ca47842
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-06-27 15:14:09 -0400

    Handlebars Added

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 //requires the express framework for this file
 const express = require("express");
-
-//requires the Client class from the PostgresSQL npm package
-const {Client} = require("pg")
-
-//creates a new instance of the PostgresSQL Client class
-const planner = new Client({
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    max: 20,
-    connectionTimeoutMillis: 0,
-    idleTimeoutMillis: 0
-})
-
-//runs the asynchronous connect method provided by the Client class
-planner.connect()
-//run after the connect method completes
-.then(() => console.log("Database Connected"))
-//runs if the .then errors out and displays the error
-.catch(e => console.log(e))
-//runs last, after the .then or .catch method has run
-.finally(() => planner.end())
-
 //creates a variable to hold the express framework
 const app = express();
+const hb = require('express-handlebars');
 
 //uses the Express use() method
 //the use() method is used to implement middleware on the server
 //middleware used to give the server access to programs front-end files
-app.use(express.static(__dirname + "/public"))
+app.use(express.static('public'));
+
+app.engine('handlebars', hb());
+app.set('view engine', 'handlebars');
+
+app.get('/calculator', (req, res) => {
+    res.render('calculator', 
+    {title: "Calculator",
+    css: "css/calculator.css",
+    js: "scripts/calculator.js"}); 
+})
+
+app.get('/calendar', (req, res) => {
+    res.render('calendar', 
+    {title: "Calendar",
+    css: "css/calendar.css",
+    js: "scripts/calendar.js"}); 
+})
+
+app.get('/map', (req, res) => {
+    res.render('map', 
+    {title: "Map",
+    css: "css/map.css",
+    js: "scripts/map.js"}); 
+})
+
+app.get('/todo', (req, res) => {
+    res.render('todo', 
+    {title: "To Do",
+    css: "css/todo.css",
+    js: "scripts/todo.js"}); 
+})
 
 //uses the Express listen() method
 //the listen() is used to run the server on the specified port
-app.listen( 3000, () => { console.log("Server Running") } )
+app.listen( 3000, () => { console.log("Server Running on port", 3000) } );
