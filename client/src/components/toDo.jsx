697e7fa63fd080983b5d5f66dbc25712e63e26df
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-08-22 15:10:46 -0400

    Refactoring React To Do List

--- client/src/components/toDo.jsx
+++ client/src/components/toDo.jsx
@@ -5,12 +5,20 @@ import PlannerAPI from '../plannerAPI.js';
 const ToDoC = () => {
 
     const [input, setInput] = useState("");
-    let toDoList = ""
-    let addButton = document.querySelector(".to-do-button");
-    let toDoKey = 0;
-    let highestKey = 0;
+    let toDoArray = [];
+    const [toDoList, setToDoList] = useState([]);
+    let highestKey = localStorage.length;
 
-
+    useEffect(() => {
+        const fetchData = async (req, res) => {
+            try{
+                getToDos();
+            }catch(err){
+                console.log(err);
+            }
+        }
+        fetchData();
+    }, []);
 
     // inputText.addEventListener("keyup", function enterToDo(){
     //     if(event.keyCode === 13){
@@ -19,90 +27,50 @@ const ToDoC = () => {
     // });
 
     function getStorageSize(){
-
         for(let i = 0; i <= localStorage.length; i++){
-
             if(parseInt(localStorage.key(i), 10) >= highestKey){
-
-                highestKey = localStorage.key(i);
-
+                highestKey = parseInt(localStorage.key(i));
             }
-
         }
-
     }
 
     function getToDos(){
-
-        getStorageSize()
-
-        for(let i = 1; i <= highestKey; i++){
-
+        getStorageSize();
+        for(let i = 1; i <= localStorage.length; i++){
             if(localStorage.getItem(i) !== null){
-
-                toDoList += '<div class="grid to-do-item"><div class="to-do-item-name">' + localStorage.getItem(i) + '</div><div id ="' + i + '" onClick={() => deleteToDo(e)} class="to-do-delete">X</div></div>';
-
+                toDoArray.push(localStorage.getItem(i));
             }
-
         }
-
-        toDoKey = highestKey;
-
+        setToDoList(toDoArray)
     }
 
-    getToDos();
-
     function addToDo(){
-
+        getToDos()
+        let newKey = highestKey + 1;
         if (input !== ""){
-
-            toDoKey++;
-
-            toDoList.innerHTML += '<div class="grid to-do-item"><div class="to-do-item-name">' + input + '</div> <div id ="' + toDoKey + '" onClick={() => deleteToDo(e)} class="to-do-delete">X</div></div>';
-
-            localStorage.setItem(toDoKey, input);
+            toDoArray.push(input);
+            localStorage.setItem(newKey, input);
+            setToDoList(toDoArray)
+            getStorageSize()
+            console.log(highestKey)
         }
-
-        input = "";
-
+        setInput("");
     };
 
     const deleteToDo = (e) => {
-        
         let elementClicked = e.target;
-
+        console.log(elementClicked)
         if(elementClicked.classList[0] === "to-do-delete"){
-
             let removedToDo = elementClicked.parentElement;
-
             removedToDo.remove();
-
             let elementRemoved = elementClicked.getAttribute('id');
-
-            console.log(elementRemoved);
-
             localStorage.removeItem(elementRemoved);
         }
-            
+        getToDos()  
     };
 
-
-
-
-
-    useEffect(() => {
-        const fetchData = async (req, res) => {
-            try{
-                // const response = await PlannerAPI.get(`/?`);
-            }catch(err){
-                console.log(err);
-            }
-        }
-        fetchData();
-    }, []);
-
     return(
-        <div classNameName="main-body">
+        <div className="main-body">
             <div className="grid grid-center">
                 <div className="grid grid-center container">
                     <div className="title">My To-Do's</div>
@@ -111,6 +79,16 @@ const ToDoC = () => {
                         <button onClick={() => addToDo()} className="to-do-button"><img src="../images/pencil-alt-solid.svg"/></button>
                     </div>
                     <div className="grid to-do-list">
+                    {toDoList.map((toDo, index) => {
+                        return(
+                            <div className="grid to-do-item" key={index}>
+                                <div className="to-do-item-name">
+                                    {toDo}
+                                </div> 
+                                <div id={highestKey} onClick={e => deleteToDo(e)} className="to-do-delete">X</div>
+                            </div>
+                        );
+                    })}
                     </div>
                 </div>
             </div>
