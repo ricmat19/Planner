13f85996e950c88f0f6291f9073c11977834ffb5
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-08-27 23:41:49 -0400

    Adding ToDo Modal

--- client/src/components/planner.jsx
+++ client/src/components/planner.jsx
@@ -1,20 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PlannerAPI from '../plannerAPI.js';
 
 
-const ToDoC = () => {
+const ToDoC = (props) => {
 
     const [input, setInput] = useState("");
     let toDoArray = [];
+    const [toDoModal, setToDoModal] = useState("modal");
     const [toDoLists, setToDoLists] = useState([]);
     const [toDos, setToDos] = useState([]);
     let highestKey = localStorage.length;
+
+    const modalRef = useRef();
 
     useEffect(() => {
         const fetchData = async (req, res) => {
             try{
                 const response = await PlannerAPI.get(`/planner`);
                 setToDoLists(response.data.data);
+
+                document.addEventListener("mousedown", (event) => {
+                    if(modalRef.current !== null){
+                        if(!modalRef.current.contains(event.target)){
+                            setToDoModal("modal");
+                        }
+                    }
+                })
 
                 // getToDos();
             }catch(err){
@@ -49,20 +60,11 @@ const ToDoC = () => {
     }
 
     const createList = (e) => {
-        
+
     };
 
     function addToDo(){
-        // getToDos()
-        // let newKey = highestKey + 1;
-        // if (input !== ""){
-        //     toDoArray.push(input);
-        //     localStorage.setItem(newKey, input);
-        //     setToDos(toDoArray)
-        //     getStorageSize()
-        //     console.log(highestKey)
-        // }
-        // setInput("");
+        setToDoModal("modal modal-active");
     };
 
     const deleteToDo = (e) => {
@@ -79,13 +81,44 @@ const ToDoC = () => {
 
     return(
         <div className="main-body">
+        
+            {/* Create List */}
+            <div className={toDoModal}>
+                <form>
+                    <div ref={modalRef} className="modal-content">
+                        <div>
+                            <input className="modal-header title todo" type="text" name="todo"/>
+                        </div>
+                        <div className="toDo-modal-grid">
+                            <label>in list</label>
+                            <input className="modal-header list" type="text" name="list"/>
+                        </div>
+                        <div className="toDo-modal-grid">
+                            <label>Description</label>
+                            <textarea className="modal-header info" type="text" name="info"/>
+                        </div>
+                        <div className="toDo-modal-grid">
+                            <label>Due Date</label>
+                            <input className="modal-header due-date" type="date" name="dueDate"/>
+                        </div>
+                        <div className="toDo-modal-grid">
+                            <label>Attachment</label>
+                            <input className="modal-header image" type="file" name="file"/>
+                        </div>
+                        <div>
+                            <button>Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
             <div className="grid grid-center align-horizontal">
                 {/* {toDos.map(toDoSet => { */}
                     <div className="grid grid-center container">
                         <div className="title">My To-Do's</div>
                         <div className="grid input-div">
                             <input onChange={e => setInput(e.target.value)} className="input-box" placeholder="Add to do..." type="text"/>
-                            <button onClick={() => addToDo()} className="to-do-button"><img src="../images/pencil-alt-solid.svg"/></button>
+                            <button onClick={addToDo} className="to-do-button"><img src="../images/pencil-alt-solid.svg"/></button>
                         </div>
                         <div className="grid to-do-list">
                         {toDos.map((toDo, index) => {
