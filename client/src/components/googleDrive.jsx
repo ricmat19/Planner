e25a847c18ab0fa8d7551ef7ffa11e3dde06eba7
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-09-12 01:06:27 -0400

    Integrating Google Drive

---
+++ client/src/components/googleDrive.jsx
@@ -0,0 +1,25 @@
+// import {google} from 'googleapis';
+// import path from 'path';
+// import fs from 'fs';
+
+// const GoogleDriveC = (props) => {
+
+// const CLIENT_ID = process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_ID;
+// const CLIENT_SECRET = process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_SECRET;
+// const REDIRECT_URI = process.env.REACT_APP_GOOGLE_DRIVE_REDIRECT_URI;
+// const REFRESH_TOKEN = process.env.REACT_APP_GOOGLE_DRIVE_REFRESH_TOKEN;
+
+// const oAuth2Client = new google.auth.OAuth2(
+//     CLIENT_ID,
+//     CLIENT_SECRET,
+//     REDIRECT_URI
+// )
+
+// oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+
+//     return(
+//         <div></div>
+//     );
+// }
+
+// export default GoogleDriveC;
