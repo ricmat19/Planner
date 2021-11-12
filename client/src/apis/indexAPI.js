e8371101cadc01d7bde5e70dec3911de73ef02d0
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-11-11 23:03:00 -0500

    Fixing Deployment

--- client/src/apis/indexAPI.js
+++ client/src/apis/indexAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000" || "https://planner-ricmat19.herokuapp.com",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://planner-ricmat19.herokuapp.com",
 });
--- database/index.js
+++ database/index.js
@@ -6,11 +6,18 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+// const db = mysql.createConnection({
+//   host: process.env.MSQLHOST || "us-cdbr-east-04.cleardb.com",
+//   user: process.env.MSQLUSER || "b140cd93893df7",
+//   password: process.env.MSQLPASSWORD || "2594891a",
+//   database: process.env.MSQLDATABASE || "heroku_f8f9e414a772132",
+// });
+
 const db = mysql.createConnection({
-  host: process.env.MSQLHOST || "us-cdbr-east-04.cleardb.com",
-  user: process.env.MSQLUSER || "b140cd93893df7",
-  password: process.env.MSQLPASSWORD || "2594891a",
-  database: process.env.MSQLDATABASE || "heroku_f8f9e414a772132",
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b140cd93893df7",
+  password: "2594891a",
+  database: "heroku_f8f9e414a772132",
 });
 
 db.connect(function (err) {
